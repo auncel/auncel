30a650b9d647e2428c75fc54d1bea48be5300c22
yidafu <tiaozuwushi@qq.com>
2019-11-24 00:29:39 +0800

    test: generate renderTree simple case

--- packages/diff-dom-core/lib/index.spec.ts
+++ packages/diff-dom-core/lib/index.spec.ts
@@ -1,9 +1,10 @@
 
 import { diff } from './diff';
 import { writeFileSync } from 'fs';
-import { genRenderTree } from './index'
-import { Puppeteer } from '@feoj/common/puppeteer/Puppeteer'
+import { genRenderTree } from './index';
+import { Puppeteer } from '@feoj/common/puppeteer/Puppeteer';
 
+// Puppeteer.getPage();
 
 it('gen Render Tree', () => {
   // genDiffRes(tree0, tree1);
