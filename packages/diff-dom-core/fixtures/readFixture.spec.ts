ba261e3f30bd42b36a77ea5e9c5df16789f85d9f
yidafu <tiaozuwushi@qq.com>
2019-11-26 00:31:48 +0800

    feat: read fixture html trans to Object

---
+++ packages/diff-dom-core/fixtures/readFixture.spec.ts
@@ -0,0 +1,20 @@
+import { readFixture, IFixtureData, readFixtures } from './readFixture';
+
+
+describe('readFixture', () => {
+  test('simlpe', async () => {
+   const simpleFixture: IFixtureData = await readFixture(__dirname + '/elements/div/simple/simple.question.html');
+   expect(simpleFixture.name).toBe('question: elements -> div -> simple -> simple');
+   expect(simpleFixture.description).toBe('最简单的 div，只有宽高');
+   expect(simpleFixture.fragment).toBe('<div></div>');
+   expect(simpleFixture.stylesheet).toBe('div {\n    width: 100px;\n    height: 100px;\n    background-color: red;\n  }');
+  });
+});
+
+describe('readFixtures', () => {
+  test('simple', async () => {
+    const fixture = await readFixtures(__dirname + '/elements/div/simple');
+    expect(fixture.question.fragment).toBe(fixture.answers[0].fragment);
+    expect(fixture.question.stylesheet).not.toBe(fixture.answers[0].stylesheet);
+  });
+});
---
+++ packages/diff-dom-core/fixtures/readFixture.ts
@@ -0,0 +1,68 @@
+import * as fs from 'fs';
+import { join, relative } from 'path';
+
+const fsp = fs.promises;
+
+const SPLITTER = '<!-- splitter -->';
+
+export interface IFixtureData {
+  name: string;
+  description: string;
+  type: string;
+  fragment: string;
+  stylesheet: string;
+}
+
+export interface IFixture {
+  question: IFixtureData;
+  answers: IFixtureData[];
+}
+
+export function readFixture(filepath): Promise<IFixtureData> {
+  if (!fs.existsSync(filepath)) {
+    throw Error(`file ${filepath} dosen't exist!`);
+  }
+
+  return fsp.readFile(filepath, { encoding: 'utf8' })
+    .then((htmlContent: string) => {
+      const commentEndIdx = htmlContent.indexOf('-->');
+      const description = htmlContent.substring(4, commentEndIdx).trim();
+      const splitterIdx = htmlContent.indexOf(SPLITTER);
+      const stylesheet = htmlContent.substring(commentEndIdx + 3, splitterIdx).trim().slice(7, -8).trim();
+      const fragment = htmlContent.substring(splitterIdx + SPLITTER.length).trim();
+
+      const relativePaths = relative(__dirname, filepath).split('/');
+      const filename = relativePaths.pop();
+
+      const [typeName, type] = filename.split('.');
+      relativePaths.push(typeName);
+      const name = `${type}: ${relativePaths.join(' -> ')}`;
+      return {
+        name,
+        description,
+        type,
+        fragment,
+        stylesheet,
+      };
+    });
+}
+
+
+export async function readFixtures(dirpath): Promise<IFixture> {
+  if (!fs.statSync(dirpath).isDirectory) {
+    throw Error(`${dirpath} must be Directory`);
+  }
+  const fixtureObject: IFixture = { question: null, answers: [] };
+  const filenames = fs.readdirSync(dirpath);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const filename of filenames) {
+    // eslint-disable-next-line no-await-in-loop
+    const fixtureData = await readFixture(join(dirpath, filename));
+    if (fixtureData.type === 'question') {
+      fixtureObject.question = fixtureData;
+    } else {
+      fixtureObject.answers.push(fixtureData);
+    }
+  }
+  return fixtureObject;
+}
