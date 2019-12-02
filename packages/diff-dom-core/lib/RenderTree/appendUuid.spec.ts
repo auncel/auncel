97c35d2abfab16c886e690713aa163fab88e455d
yidafu <tiaozuwushi@qq.com>
2019-12-02 23:11:25 +0800

    feat: append uuid to elements

---
+++ packages/diff-dom-core/lib/RenderTree/appendUuid.spec.ts
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/* --------------------------------------------------------------------------*
+ * Description:                                                              *
+ *                                                                           *
+ * File Created: Monday, 2nd December 2019 10:16 pm                          *
+ * Author: yidafu(dov-yih) (me@yidafu.dev)                                   *
+ *                                                                           *
+ * Last Modified: Monday, 2nd December 2019 10:16 pm                         *
+ * Modified By: yidafu(dov-yih) (me@yidafu.dev>)                             *
+ *                                                                           *
+ * Copyright 2019 - 2019 MIT License                                         *
+ *-------------------------------------------------------------------------- */
+
+import { getSimpleData } from '../../test/utils';
+import { eppendUuid } from './appendUuid';
+import { UUID_ATTR } from './getCSSPropertyValues';
+
+const { fragment, stylesheet } = getSimpleData();
+
+beforeEach(() => {
+  document.body.innerHTML = fragment;
+});
+
+
+describe('append UUID to Element', () => {
+  test('simple', () => {
+    eppendUuid(document);
+    expect(document.body.getAttribute(UUID_ATTR)).toBe('uuid_0');
+    expect(document.querySelector('.listItem_listItem').getAttribute(UUID_ATTR)).toBe('uuid_0_0');
+    expect((document.body.firstElementChild.lastElementChild as Element).getAttribute(UUID_ATTR)).toBe('uuid_0_0_1');
+  });
+})
