455402b3b34e288d127091d20630f0fc1e7d728d
yidafu <tiaozuwushi@qq.com>
2019-12-21 16:36:39 +0800

    feat: users can login now
    1. DI http service
    2. md5 encrypt function

--- packages/client-web/src/network/LoginService.spec.ts
+++ packages/client-web/src/network/LoginService.spec.ts
@@ -1,14 +1,25 @@
 /* --------------------------------------------------------------------------*
  * Description:                                                              *
  *                                                                           *
- * File Created: Sunday, 15th December 2019 10:34 pm                         *
+ * File Created: Saturday, 21st December 2019 11:35 am                       *
  * Author: yidafu(dov-yih) (me@yidafu.dev)                                   *
  *                                                                           *
- * Last Modified: Sunday, 15th December 2019 10:34 pm                        *
+ * Last Modified: Saturday, 21st December 2019 11:35 am                      *
  * Modified By: yidafu(dov-yih) (me@yidafu.dev>)                             *
  *                                                                           *
  * Copyright 2019 - 2019 Mozilla Public License 2.0                          *
  *-------------------------------------------------------------------------- */
-export function loginReq(): Promise<object> {
-  return Promise.resolve({});
-}
+import LoginService from './LoginService';
+import 'reflect-metadata';
+import { ReflectiveInjector, Injectable, Injector, } from 'injection-js';
+import HttpRequest from './HttpRequest';
+ 
+export const injector = ReflectiveInjector.resolveAndCreate([LoginService, HttpRequest]);
+
+describe('DI', () => {
+  test('di works', () => {
+    const loginService = injector.get(LoginService)
+    loginService.login();
+    expect(true)
+  });
+});
