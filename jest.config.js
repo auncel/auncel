ab9883d49514cbcf14eea4f160f3faae8e3e7c6b
yidafu <tiaozuwushi@qq.com>
2019-12-21 19:05:58 +0800

    chore: add ts-jest, beautify package.json

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* --------------------------------------------------------------------------*
+ * Filename: d:\Project\fe-oj\jest.config.js                                 *
+ * Path: d:\Project\fe-oj                                                    *
+ * Created Date: Saturday, December 21st 2019, 5:42:16 pm                    *
+ * Author: yidafu(dov-yih)                                                   *
+ *                                                                           *
+ * Copyright (c) 2019 None                                                   *
+ *-------------------------------------------------------------------------- */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
