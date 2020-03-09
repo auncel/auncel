9c91682da0fef4116c8a89e84dbcdc0d617df96f
yidafu <tiaozuwushi@qq.com>
2020-03-09 22:31:32 +0800

    refactor: add 2 submodule

--- packages/diff-service/src/time.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TimeMiddleware } from './time.middleware';
-
-describe('TimeMiddleware', () => {
-  it('should be defined', () => {
-    expect(new TimeMiddleware()).toBeDefined();
-  });
-});
--- packages/diff-service/src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-import { configure, getLogger } from 'log4js';
-
-configure({
-  appenders: { diff: { type: 'file', filename: 'diff.log' } },
-  categories: { default: { appenders: ['diff'], level: 'info' } },
-});
-
-export const diffLogger = getLogger('diff');
